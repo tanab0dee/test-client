9249748d4b4ecabd28949740813b1c70db0f4639
Tanabodee Boonrak <134668629+tanab0dee@users.noreply.github.com>
2023-12-12 00:21:27 +0700

    Update experience.component.ts

--- src/app/layouts/experience/experience.component.ts
+++ src/app/layouts/experience/experience.component.ts
@@ -6,9 +6,8 @@ import { Emitter } from 'src/app/emitters/emitter';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { finalize } from 'rxjs/operators';
-import { AuthInterceptor } from 'src/app/interceptors/auth.interceptor';
+
 import { PortfolioDataService } from 'src/app/service/portfolio-data.service';
-import { EnvEndpointService } from 'src/app/service/env.endpoint.service';
 
 interface ExperienceInfo {
   exp_id: string;
@@ -24,7 +23,6 @@ interface ExperienceInfo {
 export class ExperienceComponent implements OnInit {
   experience: ExperienceInfo[] = [];
   updateForm: FormGroup;
-  ENV_REST_API = `${this.envEndpointService.ENV_REST_API}`
 
   constructor(
     public dialog: MatDialog,
@@ -32,8 +30,7 @@ export class ExperienceComponent implements OnInit {
     private http: HttpClient,
     private formBuilder: FormBuilder,
     private messageService: MessageService,
-    private portfolioDataService: PortfolioDataService,
-    private envEndpointService: EnvEndpointService
+    private portfolioDataService: PortfolioDataService
   ) {
     this.updateForm = this.formBuilder.group({
       exp_id: '',
@@ -43,28 +40,11 @@ export class ExperienceComponent implements OnInit {
 
   ngOnInit(): void {
     this.fetchExperienceData();
-    this.checkLogin();
-  }
-
-  checkLogin(): void {
-    this.http.get(`${this.ENV_REST_API}/user`, { withCredentials: true })
-      .subscribe({
-        next: (res: any) => {
-          AuthInterceptor.accessToken;
-          Emitter.authEmitter.emit(true);
-        },
-        error: () => {
-          this.router.navigate(['/login']);
-          Emitter.authEmitter.emit(false);
-        }
-      });
   }
 
   fetchExperienceData(): void {
     this.portfolioDataService.getExperienceData().subscribe({
       next: (res) => {
-        AuthInterceptor.accessToken;
-        Emitter.authEmitter.emit(true);
         this.experience = res.data;
       },
       error: () => {
